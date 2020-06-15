9615ea39a48141d57ef8447cd71a4285f410eb9f
motohacy <kazutaka.motohashi@gmail.com>
2020-06-15 13:09:13 +0900

    update
    update

--- speech.js
+++ speech.js
@@ -4,14 +4,16 @@ class Speech {
 
     this.recognition;
     this.lang = 'ja-JP';
-
     this.isDebug = true;
+
+    this.onChangeStatus;
     this.onSpeech;
+    this.onSpeechEnd;
 
     this.flag_speech = 0;
   }
 
-  start() {
+  async start() {
 
     window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
     this.recognition = new webkitSpeechRecognition();
@@ -19,50 +21,47 @@ class Speech {
     this.recognition.interimResults = true;
     this.recognition.continuous = true;
 
-    this.recognition.onsoundstart = function () {
-      document.getElementById('message').innerHTML = "認識中...";
+    this.recognition.onsoundstart = () => {
+      this.onChangeStatus("認識中");
     };
      
-    this.recognition.onnomatch = function () {
-      document.getElementById('message').innerHTML = "音声を認識できませんでした";
+    this.recognition.onnomatch = () => {
+      this.onChangeStatus("音声を認識できませんでした");
     };
     
-    this.recognition.onerror = function () {
-      document.getElementById('message').innerHTML = "エラー";
+    this.recognition.onerror = () => {
+      this.onChangeStatus("エラーが発生しました");
     if (this.flag_speech == 0)
       this.start();
     };
 
-    this.recognition.onsoundend = function () {
-      document.getElementById('message').innerHTML = "停止中";
+    this.recognition.onsoundend = () => {
+      this.onChangeStatus("停止中");
       this.start();
-    };
+    };	
 
-    this.recognition.onresult = function (event) {
-       var results = event.results;
-       for (var i = event.resultIndex; i < results.length; i++) {
-          if (results[i].isFinal) {
-                    var result_transcript = results[i][0].transcript
-                    if (this.lang == 'ja-JP') {
-                        result_transcript += '。';
-                    }
-
-                    document.getElementById('result_text').innerHTML = result_transcript;
-                    document.getElementById('result_log').insertAdjacentHTML('beforeend', result_transcript+'\n');
-                    textAreaHeightSet(document.getElementById('result_log'));
-                    this.start();
-                    this.flag_speech = 0;
-                } else {
-                    var result_transcript = results[i][0].transcript;
-                    document.getElementById('result_text').innerHTML = result_transcript;
-                    this.flag_speech = 1;
-                }
-            }
+    this.recognition.onresult = (event) => {
+      var results = event.results;
+      for (var i = event.resultIndex; i < results.length; i++) {
+        if (results[i].isFinal) {
+          var result_transcript = results[i][0].transcript
+          result_transcript += "。";
+         
+          this.onSpeech(result_transcript);
+          this.onSpeechEnd(result_transcript);
+          this.start();
+          this.flag_speech = 0;
+        } else {
+          var result_transcript = results[i][0].transcript;
+          this.onSpeech(result_transcript);
+          this.flag_speech = 1;
         }
+      }
+    }
 
-        this.flag_speech = 0;
-        document.getElementById('message').innerHTML = "待機中";
-        this.recognition.start();
+    this.flag_speech = 0;
+    this.onChangeStatus("待機中");
+    this.recognition.start();
   }
 
   async stop() {
